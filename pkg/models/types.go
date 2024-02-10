3728d1fa1898298a5066a4762400a1156a0aa403
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2024-02-11 01:41:43 +0500

    add the hook without logic

--- pkg/models/types.go
+++ pkg/models/types.go
@@ -1,9 +1,21 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 type BasicAuth struct {
 	Login, Password string
 }
 
 type Answer struct {
 	Answer string `json:"answer"`
+}
+
+func (p *Pupil) BeforeUpdates(db *gorm.DB) (err error) {
+	// finish making logic
+	//r.Database.SavePoint()
+	//r.Database.Commit()
+	//r.Database.RollbackTo()
+	return nil
 }
