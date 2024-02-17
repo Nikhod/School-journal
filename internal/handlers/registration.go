2e3d7961f68bd452ca8247c75f1b7a8f53baee03
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2024-02-17 17:07:40 +0500

    change logic

--- internal/handlers/registration.go
+++ internal/handlers/registration.go
@@ -9,18 +9,37 @@ import (
 )
 
 func (h *Handler) RegistrationPupil(w http.ResponseWriter, r *http.Request) {
-	auth := models.BasicAuth{
-		Login:    r.Header.Get("login"),
-		Password: r.Header.Get("pass"),
+	err := r.ParseForm()
+	if err != nil {
+		helpers.InternalServerError(w, h.Logger, err)
+		return
 	}
 
-	err := h.Service.ValidateLoginAndPass(&auth)
+	var pupil = models.Pupil{
+		Name:        r.FormValue("name"),
+		Surname:     r.FormValue("surname"),
+		Patronymic:  r.FormValue("patronymic"),
+		Login:       r.FormValue("login"),
+		Password:    r.FormValue("password"),
+		YearOfBirth: r.FormValue("b-day"),
+		Address:     r.FormValue("address"),
+		Number:      r.FormValue("number"),
+		ExtraInfo:   r.FormValue("extra-info"),
+	}
+	schoolName := r.FormValue("school_name")
+
+	auth := models.BasicAuth{
+		Login:    pupil.Login,
+		Password: pupil.Password,
+	}
+
+	err = h.Service.ValidateLoginAndPass(&auth)
 	if err != nil {
 		helpers.BadRequest(w, h.Logger, err)
 		return
 	}
 
-	err = h.Service.RegistrationPupil(&auth)
+	err = h.Service.RegistrationPupil(&pupil, schoolName)
 	if err != nil {
 		if errors.Is(err, errors.New("the login is already used")) {
 			helpers.Forbidden(w, h.Logger, err)
@@ -30,7 +49,7 @@ func (h *Handler) RegistrationPupil(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = helpers.SendAnswer(w, "Регистрация прошла успешно")
+	err = helpers.SendAnswer(w, "registration has finished successfully")
 	if err != nil {
 		helpers.BadRequest(w, h.Logger, err)
 		return
