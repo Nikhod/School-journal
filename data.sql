c748fd3214e6087cac703ee2e597c652b7f35c42
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2024-02-19 01:44:52 +0500

    add example data for testing to tables

---
+++ data.sql
@@ -0,0 +1,75 @@
+-- insert into table subject_names
+insert into subject_names(subject_name)
+values ('‘Mathematics’');
+insert into subject_names(subject_name)
+values ('‘English Language’');
+insert into subject_names(subject_name)
+values ('‘Biology’');
+insert into subject_names(subject_name)
+values ('‘Chemistry’');
+insert into subject_names(subject_name)
+values ('‘Physics’');
+insert into subject_names(subject_name)
+values ('‘History’');
+insert into subject_names(subject_name)
+values ('‘Geography’');
+insert into subject_names(subject_name)
+values ('‘Art’');
+insert into subject_names(subject_name)
+values ('‘Physical Education’');
+insert into subject_names(subject_name)
+values ('‘Music’');
+insert into subject_names(subject_name)
+values ('‘Computer Science’');
+insert into subject_names(subject_name)
+values ('‘Economics’');
+insert into subject_names(subject_name)
+values ('‘Sociology’');
+
+
+-- insert into table teachers
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Анна’', '‘Волкова’', '‘Дмитриевна’', '21.01.1989', 'annaLogin', 'pass', '951-752-751', '71', '21', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Иван’', '‘Петров’', '‘Александрович’', '21.01.1989', 'ivanLogin', 'pass', '951-752-751', '73', '43', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Наталья’', '‘Васильева’', '‘Викторовна’', '21.01.1989', 'nataliyalogin', 'pass', '951-752-751', '95', '54',
+        '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Андрей’', '‘Зайцев’', '‘Сергеевич’', '21.01.1989', 'andreyLogin', 'pass', '951-752-751', '70', '22', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Татьяна’', '‘Павлова’', '‘Андреевна’', '21.01.1989', 'tatiyanaLogin', 'pass', '951-752-751', '79', '11', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Алексей’', '‘Иванов’', '‘Петрович’', '21.01.1989', 'alexeyLogin', 'pass', '951-752-751', '77', '42', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Мария’', '‘Смирнова’', '‘Алексеевна’', '21.01.1989', 'mariyaLlogin', 'pass', '951-752-751', '65', '14', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Виктор’', '‘Соколов’', '‘Владимирович’', '21.01.1989', 'Victorlogin', 'pass', '951-752-751', '68', '27', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Елена’', '‘Попова’', '‘Ивановна’', '21.01.1989', 'elenaLogin', 'pass', '951-752-751', '41', '28', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Сергей’', '‘Кузнецов’', '‘Андреевич’', '21.01.1989', 'sergeylogin', 'pass', '951-752-751', '32', '39', '3');
+
+insert into teachers (name, surname, patronymic, year_of_birth, login, password, number, classroom,
+                      classroom_management, school_id)
+values ('‘Ольга’', ' ‘Новикова’', '‘Сергеевна’', '21.01.1989', 'olgaLogin', 'pass', '951-752-751', '99', '49', '3');
+
